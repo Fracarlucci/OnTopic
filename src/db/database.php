0054f85725b5be75d9c10747c1009b31c29941c6
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-01-19 19:25:46 +0100

    Improve queries to cover all needs

--- src/db/database.php
+++ src/db/database.php
@@ -9,15 +9,6 @@ class DatabaseHelper{
         }        
     }
 
-    public function getRandomPosts($n){
-        $stmt = $this->db->prepare("SELECT idarticolo, titoloarticolo, imgarticolo FROM articolo ORDER BY RAND() LIMIT ?");
-        $stmt->bind_param('i',$n);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
-
     /**
     * User CRUD
     */
@@ -31,6 +22,21 @@ class DatabaseHelper{
 
         $stmt = $this->db->prepare($query);
         $stmt->bind_param('i',$userId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getUsersByUsername($slug) {
+        $query = "
+            SELECT id, username, imgProfilo 
+            FROM utente
+            WHERE username = ?
+        ";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s',$slug);
         $stmt->execute();
         $result = $stmt->get_result();
 
@@ -89,6 +95,146 @@ class DatabaseHelper{
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
+
+    public function insertPost($testo, $immagine, $tema, $utente){
+        $query = "INSERT INTO post (testo, immagine, idTema, idUtente) VALUES (?, ?, ?, ?)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ssii',$testo, $immagine, $tema, $utente);
+        $stmt->execute();
+        
+        return $stmt->insert_id;
+    }
+
+    public function updatePostById($idPost, $testo, $immagine){
+        $query = "UPDATE post SET testo = ?, immagine = ? WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ssi',$testo, $immagine, $idPost);
+        
+        return $stmt->execute();
+    }
+
+    public function incrementCommentsById($idPost){
+        $query = "UPDATE post SET commenti = commenti + 1 WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idPost);
+        
+        return $stmt->execute();
+    }
+
+    public function decrementCommentsById($idPost){
+        $query = "UPDATE post SET commenti = commenti - 1 WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idPost);
+        
+        return $stmt->execute();
+    }
+
+    public function incrementLikesById($idPost){
+        $query = "UPDATE post SET miPiace = miPiace + 1 WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idPost);
+        
+        return $stmt->execute();
+    }
+
+    public function decrementLikesById($idPost){
+        $query = "UPDATE post SET miPiace = miPiace - 1 WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idPost);
+        
+        return $stmt->execute();
+    }
+
+    public function deletePostById($idPost){
+        $query = "UPDATE post SET abilitato = 0 WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idPost);
+        $stmt->execute();
+        var_dump($stmt->error);
+        return true;
+    }
+
+    /**
+     * Comments CRUD
+     */
+
+    public function insertComment($testo, $immagine, $post, $utente){
+        $query = "INSERT INTO commento (testo, immagine, idUtente, idPost) VALUES (?, ?, ?, ?)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ssii',$testo, $immagine, $utente, $post);
+        $stmt->execute();
+        
+        return $stmt->insert_id;
+    }
+
+    public function getCommentsByPostId($idPost){
+        $query = "
+            SELECT c.id, c.dataOra, c.testo, c.immagine, u.username, u.imgProfilo 
+            FROM commento c INNER JOIN utente u ON c.idUtente = u.id INNER JOIN post p ON c.idPost = p.id
+            WHERE p.id = ?
+        ";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idPost);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    /**
+     * Likes CRUD
+     */
+
+    public function getLikesByPostId($idPost){
+        $query = "
+            SELECT u.id, u.username, u.imgProfilo
+            FROM mi_piace m INNER JOIN utente u ON m.idUtente = u.id
+            WHERE m.idPost = ?
+        ";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idPost);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function insertLike($idPost, $idUtente){
+        $query = "INSERT INTO mi_piace (idPost, idUtente) VALUES (?, ?)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ii',$idPost, $idUtente);
+        $stmt->execute();
+        
+        return $stmt->insert_id;
+    }
+
+    /**
+     * Theme CRUD
+     */
+
+    public function getThemes($n=-1){
+        $query = "
+            SELECT data, nome
+            FROM tema
+        ";
+        if($n > 0){
+            $query .= " LIMIT ?";
+        }
+        $stmt = $this->db->prepare($query);
+        if($n > 0){
+            $stmt->bind_param('i',$n);
+        }
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+    
+    /**
+     * Login
+     */
 
     public function checkLogin($username, $password){
         $query = "SELECT idautore, username, nome FROM autore WHERE attivo=1 AND username = ? AND password = ?";
