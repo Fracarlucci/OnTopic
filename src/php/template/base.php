f63bcde2c4c271f16c52269c22117c8228e9e2b5
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2023-01-19 23:39:58 +0100

    fix aside title alignment

--- src/php/template/base.php
+++ src/php/template/base.php
@@ -39,7 +39,7 @@
                     <nav>   
                         <ul class="nav nav-pills" role="tablist">
                             <!-- Bell icon -->
-                            <div class="d-flex justify-content-start col-3">
+                            <div class="d-flex align-items-center col-3">
                                 <li>
                                     <!-- Logo Desktop-->
                                     <div id="logoDesktop" class="text-left">
@@ -90,45 +90,57 @@
                     <aside class="m-2 px-2 py-3">
                         <!-- Bell icon -->
                         <a href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
-                                <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
-                            </svg>
-                            <h2>Notifiche</h2>
+                            <div id="notificationBell" class="col-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 21 21">
+                                    <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
+                                </svg>
+                            </div>
+                            <div id="notificationBell" class="col-5">
+                                <h2>Notifiche</h2>
+                            </div> 
                         </a>
                         <div class="bg-light border border-dark px-2 py-3 my-1 rounded">
-                            <ul>
-                                <?php foreach($templateParams["notifiche"] as $notifica): ?>
-                                    <li>
-                                        <?php if(isset($notifica["postId"])): ?>
-                                            <a href="postlink">
-                                        <?php else: ?>
-                                            <a href="userlink">
-                                        <?php endif; ?>
-                                        <?php echo $notifica["username"], $notifica["testo"]; ?></a>
-                                    </li>
-                                <?php endforeach; ?>    
-                            </ul>
+                            <nav>
+                                <ul>
+                                    <?php foreach($templateParams["notifiche"] as $notifica): ?>
+                                        <li>
+                                            <?php if(isset($notifica["postId"])): ?>
+                                                <a href="postlink">
+                                            <?php else: ?>
+                                                <a href="userlink">
+                                            <?php endif; ?>
+                                            <?php echo $notifica["username"], $notifica["testo"]; ?></a>
+                                        </li>
+                                    <?php endforeach; ?>    
+                                </ul>
+                            </nav>
                         </div>
                     </aside>
                 </div>
                 <div class="col-md-6 col-sm-12">
+                    
                     <main>
                         <?php
                         if(isset($templateParams["contenuto"])):
                             require($templateParams["contenuto"]);
                         endif; ?>
                     </main>
+                
                 </div>
                 <div class="col-3">
                     <aside class="m-2 px-2 py-3">
                         <!-- Friends icon -->
                         <a href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
-                                <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-                            </svg>
-                            <h2>Amici</h2>
+                            <div id="notificationBell" class="col-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 21 21">
+                                    <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+                                </svg>
+                            </div>
+                            <div id="friendIcon" class="col-5">
+                                <h2>Amici</h2>
+                            </div> 
                         </a>
-                        <div class="bg-light border border-dark px-2 py-3 my-1">
+                        <div class="bg-light border border-dark px-2 py-3 my-1 rounded">
                             <nav>
                                 <ul>
                                     <?php foreach($templateParams["amici"] as $amico): ?>
