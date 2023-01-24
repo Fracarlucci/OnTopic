ac63a1bc987a2b2370b53e2ae8f212c678b25c6c
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2023-01-24 18:23:27 +0100

    Add search page

--- src/cerca.php
+++ src/cerca.php
@@ -2,13 +2,13 @@
 require_once 'bootstrap.php';
 
 //Base Template
-$templateParams["titolo"] = "OnTopic - Profilo";
-$templateParams["contenuto"] = "profilo-template.php";
-$templateParams["post"] = "post-template.php";
+$templateParams["titolo"] = "OnTopic - Cerca";
+$templateParams["contenuto"] = "cerca-template.php";
+$templateParams["amici-section"] = "lista-amici.php";
 $templateParams["notifiche"] = $dbh->getNotificationsById(1);
 $templateParams["amici"] = $dbh->getUserFriendsById(1);
 $templateParams["utente"] = $dbh->getUserById(1);
-$templateParams["posts"] = $dbh->getPostsbyId(1);
+// $templateParams["risultati"] = $dbh->searchUsers();
 
 require 'template/base.php';
 ?>
