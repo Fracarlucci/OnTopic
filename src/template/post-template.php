c504e6d77ffe3e066a5966ed1d0779e10eb12c39
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2023-01-26 19:59:40 +0100

    Add like animation

--- src/template/post-template.php
+++ src/template/post-template.php
@@ -43,7 +43,7 @@
                 </svg>
             </div>
             <!-- Heart icon -->
-            <div id="heart" class="mx-1">
+            <div class="heart mx-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                 </svg>
