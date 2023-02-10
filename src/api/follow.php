ceeb5fe67674dd14dc7c212d38fe74fe8f3176dc
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-02-10 09:53:04 +0100

    Complete follow logic with session id

--- src/api/follow.php
+++ src/api/follow.php
@@ -1,20 +1,21 @@
 <?php 
     include '../db/database.php';
+    include "../utils/functions.php";
+
+    sec_session_start();
 
     $dbh = new DatabaseHelper("localhost", "root", "", "ontopic", 3306);
 
-    $username = $_POST["username"];
+    $userId = $_POST["userId"];
     $remove = false;
     if(isset($_POST["remove"])) {
         $remove = $_POST["remove"];
     }
 
-    $userId = $dbh->getUsersByUsername($username);
-
     if($remove){
-        $dbh->unfollowUser(2, $userId);   // ($_SESSION["id"], $userId)
+        $dbh->unfollowUser($_SESSION["user_id"], $userId);   // ($_SESSION["id"], $userId)
     } else {
-        $dbh->followUser(2, $userId);     // ($_SESSION["id"], $userId)
+        $dbh->followUser($_SESSION["user_id"], $userId);     // ($_SESSION["id"], $userId)
     }
     $result["seguaci"] = count($dbh->getSeguaciById($userId));
 
--- src/js/follow.js
+++ src/js/follow.js
@@ -1,9 +1,11 @@
-let username = window.location.pathname;
-username = username.split("=").pop();
-username = "Ciccio";
+//get user id to follow by query param
+const params = new URLSearchParams(window.location.search)
+userIdToFollow = params.get("id")
+
+//check if user is already followed, if not follow it
 const button = document.getElementById("seguiButton");
 let formData = new FormData();
-formData.append('username', username);
+formData.append('userId', userIdToFollow);
 
 axios.post('./api/checkSegui.php', formData).then(response => {
     if(response.data["followed"] == true){
@@ -18,14 +20,14 @@ function segui(button) {
     if(button.classList.contains("followed")){
         button.classList.remove("followed");
         formData = new FormData();
-        formData.append('username', username);
+        formData.append('userId', userIdToFollow);
         formData.append('remove', true);
         follow(formData, button, "Segui")
     }
     else if(!button.classList.contains("followed")){
         button.classList.add("followed");
         formData = new FormData();
-        formData.append('username', username);
+        formData.append('userId', userIdToFollow);
         follow(formData, button, "Segui già");
     }
 }
