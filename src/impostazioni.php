fe9de7fc661706f5da37714c5437f2877133d6e1
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-02-13 18:16:14 +0100

    Add security check into profile settings page

--- src/impostazioni.php
+++ src/impostazioni.php
@@ -2,7 +2,7 @@
 require_once 'base-without-posts.php';
 
 //redirect if not auth
-if(!$templateParams["isAuth"]){
+if(!$templateParams["isAuth"] || !isset($_GET["id"]) || $loggedUserId != $_GET["id"]){
     header('Location: index.php');
 }
 
